8b835c6681a87cf741b667f1214c1886976faa21
Nick Gregory <computerfreak97@gmail.com>
2020-03-16 21:42:38 -0400

    include patched afl

--- copy.sh
+++ copy.sh
@@ -11,3 +11,5 @@ popd
 
 scp -i wheezy.img.key -P 10022 snapshot/snapshotko.ko root@localhost:
 scp -i wheezy.img.key -P 10022 test/test root@localhost:
+scp -i wheezy.img.key -P 10022 -r perf-fuzz/afl root@localhost:
+scp -i wheezy.img.key -P 10022 -r perf-fuzz/afl-test root@localhost:
--- setup.sh
+++ setup.sh
@@ -6,7 +6,3 @@ cd "$(dirname "$0")"
 stat wheezy.img || wget https://storage.googleapis.com/syzkaller/wheezy.img
 stat wheezy.img.key || wget https://storage.googleapis.com/syzkaller/wheezy.img.key
 chmod 600 wheezy.img.key
-
-pushd snapshot
-make
-popd
