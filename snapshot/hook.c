a313269614ee9b6eba334d66e00308c13e7f5d20
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-04-03 15:14:29 +0200

    code format

--- snapshot/hook.c
+++ snapshot/hook.c
@@ -1,45 +1,54 @@
 #include <linux/kernel.h>
-#include <linux/module.h>
 #include <linux/kprobes.h>
+#include <linux/module.h>
 #include <linux/slab.h>
 
 struct hook {
-    struct kprobe kp;
-    struct list_head l;
+
+  struct kprobe kp;
+  struct list_head l;
+
 };
 
 LIST_HEAD(hooks);
 
+int try_hook(const char *func_name, void *handler) {
 
-int try_hook(const char *func_name, void *handler)
-{
-    struct hook *hook = kmalloc(sizeof(struct hook), GFP_KERNEL | __GFP_ZERO);
-    INIT_LIST_HEAD(&hook->l);
-    hook->kp.symbol_name = func_name;
-    hook->kp.pre_handler = handler;
+  struct hook *hook = kmalloc(sizeof(struct hook), GFP_KERNEL | __GFP_ZERO);
+  INIT_LIST_HEAD(&hook->l);
+  hook->kp.symbol_name = func_name;
+  hook->kp.pre_handler = handler;
 
-    int ret = register_kprobe(&hook->kp);
-    if (!ret) {
-        list_add(&hook->l, &hooks);
-    }
+  int ret = register_kprobe(&hook->kp);
+  if (!ret) {
 
-    return true;
+    list_add(&hook->l, &hooks);
+
+  }
+
+  return true;
+
 }
 
-void unhook(const char *func_name)
-{
-    struct hook *hook = NULL;
-    list_for_each_entry(hook, &hooks, l) {
-        if (!strcmp(hook->kp.symbol_name, func_name)) {
-            unregister_kprobe(&hook->kp);
-        }
+void unhook(const char *func_name) {
+
+  struct hook *hook = NULL;
+  list_for_each_entry(hook, &hooks, l) {
+
+    if (!strcmp(hook->kp.symbol_name, func_name)) {
+
+      unregister_kprobe(&hook->kp);
+
     }
+
+  }
+
 }
 
-void unhook_all(void)
-{
-    struct hook *hook = NULL;
-    list_for_each_entry(hook, &hooks, l) {
-        unregister_kprobe(&hook->kp);
-    }
+void unhook_all(void) {
+
+  struct hook *hook = NULL;
+  list_for_each_entry(hook, &hooks, l) { unregister_kprobe(&hook->kp); }
+
 }
+
--- test/test.c
+++ test/test.c
@@ -5,18 +5,17 @@
  * wxu
  */
 #define _GNU_SOURCE
+#include <fcntl.h>
+#include <malloc.h>
 #include <setjmp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <malloc.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
 #include <sys/syscall.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 void *mmm;
 unsigned long a;
@@ -24,135 +23,149 @@ unsigned long snapshot_arg[3];
 jmp_buf env;
 
 void procmap() {
-	char buf[100];
-	memset(buf, 0, sizeof(buf));
-	sprintf(buf, "cat /proc/%d/maps", getpid());
-	system(buf);
+
+  char buf[100];
+  memset(buf, 0, sizeof(buf));
+  sprintf(buf, "cat /proc/%d/maps", getpid());
+  system(buf);
+
 }
 
 void filemap() {
-	char buf[100];
-	memset(buf, 0, sizeof(buf));
-	sprintf(buf, "ls -al /proc/%d/fd", getpid());
-	system(buf);
+
+  char buf[100];
+  memset(buf, 0, sizeof(buf));
+  sprintf(buf, "ls -al /proc/%d/fd", getpid());
+  system(buf);
+
 }
 
 void worker() {
-	void *p[0x20];
-	unsigned long addr;
-	struct mallinfo mi;
-	int i, fd;
 
-	*(unsigned long *)mmm = 0x4242424242424242;
+  void *p[0x20];
+  unsigned long addr;
+  struct mallinfo mi;
+  int i, fd;
 
-	a = 0x87654321;
-	printf("a: 0x%08lx\n", a);
+  *(unsigned long *)mmm = 0x4242424242424242;
 
-	// *(unsigned long *)(0x602020) = reserved;
+  a = 0x87654321;
+  printf("a: 0x%08lx\n", a);
 
-	// this will trigger demanding paging (do_anonymous_page)
-	for (i = 0; i < 0x20; i++) {	
-		p[i] = malloc(0x100);	
-		printf("allocate: 0x%08lx\n", (unsigned long)p[i]);
-	}
-	for (i = 0; i < 0x20; i += 2)
-		free(p[i]);
-	mi = mallinfo();
-	printf("[worker] allocated bytes: %d free chunks: %d\n", mi.uordblks, mi.ordblks);
+  // *(unsigned long *)(0x602020) = reserved;
 
-	addr = (unsigned long)mmap((void *)0xa00000, 
-								0x100000, 
-								PROT_READ | PROT_WRITE | PROT_EXEC,
-								MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS, 
-								-1, 
-								0);
-	printf("mmap addr: 0x%08lx\n", addr);
+  // this will trigger demanding paging (do_anonymous_page)
+  for (i = 0; i < 0x20; i++) {
 
-	fd = open("file", O_CREAT);
-	printf("fd: %d\n", fd);
-	
-	procmap();
-	filemap();
+    p[i] = malloc(0x100);
+    printf("allocate: 0x%08lx\n", (unsigned long)p[i]);
 
-	printf("start sleeping");
-	fflush(stdout);
+  }
 
-	/*
-	while(1) {
-		sleep(1);
-		printf(".");
-		fflush(stdout);
-	}
-	*/
+  for (i = 0; i < 0x20; i += 2)
+    free(p[i]);
+  mi = mallinfo();
+  printf("[worker] allocated bytes: %d free chunks: %d\n", mi.uordblks,
+         mi.ordblks);
 
-	exit(0);
+  addr = (unsigned long)mmap((void *)0xa00000, 0x100000,
+                             PROT_READ | PROT_WRITE | PROT_EXEC,
+                             MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS, -1, 0);
+  printf("mmap addr: 0x%08lx\n", addr);
 
-	return;
+  fd = open("file", O_CREAT);
+  printf("fd: %d\n", fd);
+
+  procmap();
+  filemap();
+
+  printf("start sleeping");
+  fflush(stdout);
+
+  /*
+  while(1) {
+
+          sleep(1);
+          printf(".");
+          fflush(stdout);
+
+  }
+
+  */
+
+  exit(0);
+
+  return;
+
 }
 
-void cleanup() {	
-	/* cleanup snapshot */
-	puts("----- calling end snapshot -----");
-	syscall(__NR_tuxcall, 1, 0);
+void cleanup() {
 
-	/* jump back to the starting point */
-	longjmp(env, 0);
+  /* cleanup snapshot */
+  puts("----- calling end snapshot -----");
+  syscall(__NR_tuxcall, 1, 0);
+
+  /* jump back to the starting point */
+  longjmp(env, 0);
+
 }
 
 void test1() {
-	void *p;
-	struct mallinfo mi;
-	int i = 0;
 
-	p = malloc(0x100);
-	procmap();
-	filemap();
+  void *p;
+  struct mallinfo mi;
+  int i = 0;
 
-	a = 0x12345678;
-	printf("&a: 0x%08lx a: 0x%08lx\n", 
-			(unsigned long)(&a), a);
-	mi = mallinfo();
-	printf("allocated bytes: %d free chunks: %d\n", mi.uordblks, mi.ordblks);
+  p = malloc(0x100);
+  procmap();
+  filemap();
 
-	mmm = mmap((void *)0xa000000, 0x110000, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED | MAP_FIXED | MAP_ANONYMOUS, 0, 0);
-	printf("0x%08lx\n", (unsigned long)mmm);
-	*(unsigned long *)mmm = 0x4141414141414141;
+  a = 0x12345678;
+  printf("&a: 0x%08lx a: 0x%08lx\n", (unsigned long)(&a), a);
+  mi = mallinfo();
+  printf("allocated bytes: %d free chunks: %d\n", mi.uordblks, mi.ordblks);
 
-	/***** start snapshot *****/	
-	snapshot_arg[0] = (unsigned long)(&cleanup);
-	snapshot_arg[1] = (unsigned long)mmm;
-	snapshot_arg[2] = 0x110000;
-	setjmp(env);
+  mmm = mmap((void *)0xa000000, 0x110000, PROT_READ | PROT_WRITE | PROT_EXEC,
+             MAP_SHARED | MAP_FIXED | MAP_ANONYMOUS, 0, 0);
+  printf("0x%08lx\n", (unsigned long)mmm);
+  *(unsigned long *)mmm = 0x4141414141414141;
 
-	if (i > 5)
-		goto out;
-	i++;
+  /***** start snapshot *****/
+  snapshot_arg[0] = (unsigned long)(&cleanup);
+  snapshot_arg[1] = (unsigned long)mmm;
+  snapshot_arg[2] = 0x110000;
+  setjmp(env);
 
-	/***** start snapshot *****/
-	syscall(__NR_tuxcall, 0, snapshot_arg); 
+  if (i > 5)
+    goto out;
+  i++;
 
-	/***** do fuzzing! *****/
-	worker();
+  /***** start snapshot *****/
+  syscall(__NR_tuxcall, 0, snapshot_arg);
 
-	/***** end snapshot *****/
-	cleanup();
+  /***** do fuzzing! *****/
+  worker();
+
+  /***** end snapshot *****/
+  cleanup();
 
 out:
-	printf("mmm: 0x%08lx\n", *(unsigned long *)mmm);
-	printf("&a: 0x%08lx a: 0x%08lx\n", 
-			(unsigned long)(&a), a);
-	mi = mallinfo();
-	printf("allocated bytes: %d free chunks: %d\n", mi.uordblks, mi.ordblks);
-	procmap();
-	filemap();
+  printf("mmm: 0x%08lx\n", *(unsigned long *)mmm);
+  printf("&a: 0x%08lx a: 0x%08lx\n", (unsigned long)(&a), a);
+  mi = mallinfo();
+  printf("allocated bytes: %d free chunks: %d\n", mi.uordblks, mi.ordblks);
+  procmap();
+  filemap();
+
 }
 
 int main() {
 
-	test1();
+  test1();
 
-	unlink("file");
+  unlink("file");
 
-	return 0;
+  return 0;
+
 }
 
